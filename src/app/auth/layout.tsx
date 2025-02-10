a81c88e3f1bcdb7c99f8977008f8a74cc5fe9344
Randall Alquicer <randall.bposeats@gmail.com>
2025-02-10 14:01:18 +0800

    finshies rough draft, up for qa review

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -5,12 +5,12 @@ const Layout = () => {
         style={{ backgroundImage: `url("/auth_bg.png")` }}
       >
         {/* Sidebar */}
-        <div className="absolute left-[30vw] w-1/3 bg-black p-6 flex items-center justify-center">
+        <div className="absolute left-[15vw] w-1/4 h-[75vh] bg-black/50 p-6 flex items-center justify-left rounded-xl">
           <img src="/logo.png" alt="Logo" className="max-w-full h-auto" />
         </div>
   
         {/* Main Content */}
-        <div className="absolute left w-2/3 bg-white p-8 rounded-xl shadow-lg"></div>
+        <div className="absolute left-[35vw] w-1/2 h-[80vh] bg-white p-8 rounded-xl shadow-lg"></div>
       </div>
     );
   };
