63fc431d357eafbf9040f46390c9ce1f2af247ab
Lanieza <lblopez@up.edu.ph>
2025-02-13 08:53:19 +0800

    Apply customizable width

--- src/components/BaseButton.tsx
+++ src/components/BaseButton.tsx
@@ -24,7 +24,7 @@ export default function Button({
         bg-${color} text-${textColor} font-semibold transition hover:opacity-80  
         h-[2rem] text-sm rounded-[0.375rem]  
         sm:h-[2.5rem] sm:text-base sm:rounded-[0.4375rem]  
-        md:h-[3rem] md:text-lg md:rounded-[0.5rem]  
+        md:h-[3.0rem] md:text-lg md:rounded-[0.5rem]  
         lg:h-[3.5rem] lg:text-xl lg:rounded-[0.5625rem]  
         xl:h-[4rem] xl:text-2xl xl:rounded-[0.625rem]  
       `}
