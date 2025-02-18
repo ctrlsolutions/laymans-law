f6adac88d79829a7e682b2d2ded2c749d06a53d1
ndb3llo <ndbello@up.edu.ph>
2025-02-18 18:52:06 +0800

    [feat][Nico] Fixes json file problem, bugs, and breakpoints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,49 @@
 import Image from "next/image";
-import SignUpButtonChoices from "@/app/auth/signup/choice/SignUpChoiceButton";
+import LandingNavButton from "@/components/Global/LandingNavButton";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <SignUpButtonChoices title="Lawyer" description="I am a lawyer" variant="layman" />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      
+      <div
+        className="relative min-h-screen bg-cover bg-center"
+        style={{ backgroundImage: "url(/lowerBG.png)" }}
+      >
+     
+        <div
+          className="absolute inset-0 flex items-center bg-cover bg-center bg-fixed h-full"
+          style={{ backgroundImage: "url(/upperBG.png)" }}
+        >
+          <div className="w-1/2 sm:w-1/2 m:w-1/2 lg:2/3 flex flex-col">
+            
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/LLLogo.png"
+              alt="Layman's Law Logo"
+              width={600}
+              height={200}
+              className="w-25 sm:w-25 m:w-25 lg:w-25 pl-14 sm:pl-14 lg:pl-32 "
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+
+              <div className="mt-6 text-sm sm:text-sm m:text-m lg:text-lg pl-28 sm:pl-28 md:pl-32 lg:pl-52" >
+                Intindihin ang mga <span className="font-bold text-blue">BATAS</span><br />
+                Alamin ang iyong mga <span className="font-bold text-red">KARAPATAN</span>
+              </div>
+
+
+              <div className="mt-12 flex sm:flex-row m:flex-row gap-5 lg:gap-7  text-sm sm:text-sm m:text-m lg:text-lg pl-28 sm:pl-28 md:pl-32 lg:pl-52 pr-2 sm:pr-2 md:pr-2 lg:pr-1 xl:pr-28">
+                <LandingNavButton text="LOG IN" route="/auth/login" variant="red" width="8rem" height="3rem" fontSize="1rem" />
+                <LandingNavButton text="SIGN UP" route="/auth/signup" variant="blue" width="8rem" height="3rem" fontSize="1rem" />
+              </div>
+
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </>
   );
 }
