c0419b7efea4e0e73df8a6b53de02723d770c13c
Randall Alquicer <randall.bposeats@gmail.com>
2025-03-10 02:43:12 +0800

    styles choice

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -10,34 +10,41 @@ const SignUpPage = () => {
       className="min-h-screen flex items-center justify-center bg-cover bg-center"
       style={{ backgroundImage: `url("/auth_bg.png")` }}
     >
-      <div className="bg-white rounded-2xl shadow-lg p-8 max-w-md w-full text-center">
-        <h1 className="text-3xl font-bold mb-4">Sign Up</h1>
-        <p className="text-gray-600 mb-6">
+      <div className="bg-white rounded-2xl shadow-lg p-8 w-[40vw] text-black">
+        <h1 className="text-[50px] font-black tracking-tight mt-1">Sign Up</h1>
+        <p className="mb-6">
           Before proceeding, please choose between the two options below.
         </p>
 
-        <div className="space-y-4">
+        <div className="space-y-4 text-white">
           <button
             onClick={() => router.push("/auth/signup/lawyer")}
-            className="w-full bg-red text-white py-4 rounded-lg flex items-center justify-between px-6"
+            className="w-full bg-red py-4 rounded-lg flex items-center justify-between px-6"
           >
-            <span className="font-semibold">I am a lawyer</span>
-            <span className="text-xl">→</span>
+            <div className="flex flex-col items-start text-[3vh]">
+              <span className="font-semibold">I am a lawyer</span>
+              <p className=" text-[1.5vh] text-sm italic text-left">
+                I want to help people with regards to legal matters.
+              </p>
+            </div>
+            <span className="text-xl text-[5vh]">→</span>
           </button>
-          <p className="text-sm text-gray-500 italic">
-            I want to help people with regards to legal matters.
-          </p>
+          
 
           <button
             onClick={() => router.push("/auth/signup/layman")}
-            className="w-full bg-indigo-900 text-white py-4 rounded-lg flex items-center justify-between px-6"
+            className="w-full bg-indigo-900 py-4 rounded-lg flex items-center justify-between px-6"
           >
-            <span className="font-semibold">I am not a lawyer (layman)</span>
-            <span className="text-xl">→</span>
+            <div className="flex flex-col items-start">
+              <span className="font-semibold text-[3vh]">I am not a lawyer (layman)</span>
+              <p className="text-[1.5vh] italic text-left">
+                I would like to seek guidance/advice from lawyers regarding various concerns and problems.
+              </p>
+            </div>
+            
+            <span className="text-xl text-[5vh]">→</span>
           </button>
-          <p className="text-sm text-gray-500 italic">
-            I would like to seek guidance/advice from lawyers regarding various concerns and problems.
-          </p>
+          
         </div>
       </div>
     </div>
