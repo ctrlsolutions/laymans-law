4c9fb9099810cf3d4c66e7b91f814e6bf88b73a3
Randall Alquicer <randall.bposeats@gmail.com>
2025-03-12 02:53:44 +0800

    adds design for notifs

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -10,7 +10,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <div className="absolute left-0 top-1/2 transform -translate-y-1/2 h-[88vh] w-[320px] bg-blue/30 ml-8 p-6 rounded-3xl">
           this is nav
         </div>
-        <div className="h-[95vh] w-[90vw] bg-white p-10 pt-5 rounded-xl shadow-lg z-10 ml-[350px] mr-[20px]">{children}</div>
+        <div className="h-[95vh] w-[90vw] bg-white p-10 pt-5 rounded-xl shadow-lg z-10 ml-[25vw] mr-[20px]">{children}</div>
       </div>
     );
   };
