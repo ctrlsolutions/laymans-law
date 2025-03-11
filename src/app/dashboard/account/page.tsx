404544b0cf2c447aed057af846470ef8739514f8
Randall Alquicer <randall.bposeats@gmail.com>
2025-03-11 12:05:15 +0800

    migrates angels progress

--- src/app/dashboard/account/page.tsx
+++ src/app/dashboard/account/page.tsx
@@ -1,7 +1,12 @@
 // this is used by layout.tsx
+import UserProfile from "./Information";
 
 const Home = () => {
-    return <h1>this is account</h1>
+    
+    return (
+        <><UserProfile /><h1>this is account</h1></>
+    );
+    
 }
 
 export default Home;
