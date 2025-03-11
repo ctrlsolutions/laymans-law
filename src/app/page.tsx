573cfbf63563e13834f7c3957e63a552a65591ff
ndb3llo <ndbello@up.edu.ph>
2025-03-11 17:05:18 +0800

    [feat][Nico] Creaates Active Cases Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import LawyerNotificationsComponent from "@/components/LawyerNotificationsComponent";
+import LawyerActiveCasesComponent from "@/components/LawyerActiveCasesComponent";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
-      <LawyerNotificationsComponent />
+      <LawyerActiveCasesComponent />
     </main>
   );
 }
