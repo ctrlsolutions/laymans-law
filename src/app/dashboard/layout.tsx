012dbd907504ae779ed13bc3e86c923b3540e593
Randall Alquicer <randall.bposeats@gmail.com>
2025-03-13 13:35:13 +0800

    improves code for navbar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,54 +1,39 @@
 // this wraps everything under dashboard
+"use client";
 import Link from "next/link";
+import { useState } from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+
+    //handle active tab highlight
+    const tabs = ['Home', 'Case', 'Forum', 'Wiki', 'Account', 'Settings'];
+    const[ activeTab, setTab ] = useState('Account'); // get default from url
+    const onNavClick = (tab: string) => {
+        setTab(tab);
+    }
+
+
     return (
       <div
         className="flex h-screen items-center bg-cover bg-center relative"
         style={{ backgroundImage: `url("/bg_blue.png")` }}
       >
         {/* Sidebar */}
+
         <div className="absolute left-0 top-1/2 transform -translate-y-1/2 h-[88vh] w-[22vw] bg-blue/30 ml-8 p-6 pl-0 rounded-3xl flex flex-col justify-between">
           <img src="/logo.png" alt="Logo" className="max-w-full h-auto m-3" />
           {/* navlinks */}
           <div className="flex flex-col text-center">
-          <Link 
-            href="/" 
-            className="w-full h-[5vh] p-6 bg-white text-black hover:underline flex items-center justify-center ml-2 rounded-l-3xl"
-          >
-            Home
-          </Link>
-
-            <Link 
-              href="/about" 
-              className="w-full h-[5vh] p-5 hover:underline flex items-center justify-center ml-2 rounded-l-3xl"
-            >
-              Case
-            </Link>
-            <Link 
-              href="/about" 
-              className="w-full h-[5vh] p-5 hover:underline flex items-center justify-center ml-2 rounded-l-3xl"
-            >
-              Forum
-            </Link>
-            <Link 
-              href="/about" 
-              className="w-full h-[5vh] p-5 hover:underline flex items-center justify-center ml-2 rounded-l-3xl"
-            >
-              Wiki
-            </Link>
-            <Link 
-              href="/about" 
-              className="w-full h-[5vh] p-5 hover:underline flex items-center justify-center ml-2 rounded-l-3xl"
-            >
-              Account
-            </Link>
-            <Link 
-              href="/about" 
-              className="w-full h-[5vh] p-5 hover:underline flex items-center justify-center ml-2 rounded-l-3xl"
-            >
-              Settings
-            </Link>
+            {tabs.map((tab, index) => (
+              <Link 
+                href={`/${tab.toLowerCase()}`} 
+                key={index}
+                className={`w-full h-[5vh] p-5 hover:underline flex items-center justify-center ml-2 rounded-l-3xl ${activeTab === tab ? 'bg-white text-black' : 'bg-blue/30 text-white'}`}
+                onClick={() => onNavClick(tab)}
+              >
+                {tab}
+              </Link>
+            ))}
           </div>
           <button className=""> Logout</button>
         </div>
