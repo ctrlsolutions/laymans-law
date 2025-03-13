d6cf7577386b637ca60d7899b3fae2217b7c4d86
royalsprites ‎ <aaasenoron@gmail.com>
2025-03-13 17:59:57 +0800

    [feat][Angel] edited text size

--- src/app/dashboard/account/Information.tsx
+++ src/app/dashboard/account/Information.tsx
@@ -24,10 +24,10 @@ const UserProfile: React.FC = () => {
             className="w-[50%] h-auto rounded-transparent -mt-[50%]"
           />
         <div className="mt-2 w-full text-center">
-          <h1 className="text-l font-bold text-black truncate">{user.name}</h1>
+          <h1 className="text-xl font-bold text-black truncate">{user.name}</h1>
           <p className="text-l font-bold text-red overflow-hidden">{user.role}</p>
         </div>
-        <div className="mt-2 text-left text-sm text-black space-y-3 truncate">
+        <div className="mt-2 text-left text-sm text-black font-bold space-y-3 truncate">
           <p className="flex items-center gap-2"><FaMapMarkerAlt className="text-sm" />{user.address}</p>
           <p className="flex items-center gap-2"><FaEnvelope className="text-sm" /><a href={`mailto:${user.email}`} className="hover:underline">{user.email}</a></p>
           <p className="flex items-center gap-2"><FaPhone className="text-sm" /><a href={`tel:${user.phone}`} className="hover:underline">{user.phone}</a></p>
