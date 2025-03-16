c44f4bd1c0da051cf3d096a23b305bf7c837c2aa
Az <arbusinos@up.edu.ph>
2025-03-16 10:13:23 +0800

    [refactor][az] majorly refactors sign up page to use parallel routes for conditional routing

---
+++ src/app/(auth)/signup/@layman/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import SignupForm from "../../../../components/Auth/SignUpForm";
+import Container from "@/components/Auth/AuthContainer";
+
+export default function Home() {
+  return (
+    <Container bgColor="red">
+      <div className="flex items-center justify-center h-full">
+        <SignupForm fontColor="red" />
+      </div>
+    </Container>
+  );
+}
