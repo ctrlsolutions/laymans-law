41f9212e71efae919c7e469bcf44af607fbd5bf0
Az <arbusinos@up.edu.ph>
2025-03-20 16:19:35 +0800

    [refactor][az] refactors form components

---
+++ src/components/Global/BaseButton.tsx
@@ -0,0 +1,28 @@
+import { FormButtonProps } from "@/interface/ComponentTypes";
+
+export default function Button({
+  children,
+  color = "red",
+  textColor = "white",
+  width,
+  onClick,
+  type = "button",
+}: FormButtonProps) {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      style={width ? { width } : {}}
+      className={`
+        bg-${color} text-${textColor} font-semibold transition hover:opacity-80  
+        h-[2rem] text-sm rounded-[0.375rem]  
+        sm:h-[2.5rem] sm:text-base sm:rounded-[0.4375rem]  
+        md:h-[3.0rem] md:text-lg md:rounded-[0.5rem]  
+        lg:h-[3.5rem] lg:text-xl lg:rounded-[0.5625rem]  
+        xl:h-[4rem] xl:text-2xl xl:rounded-[0.625rem]  
+      `}
+    >
+      {children}
+    </button>
+  );
+}
