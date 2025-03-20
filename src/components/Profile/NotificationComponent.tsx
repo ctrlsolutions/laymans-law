ebbdc6c9f7f54b0bc6ef9c2eb7b14aeb1425d6e9
Caleb <calebjosh.berandoy@gmail.com>
2025-03-20 16:05:47 +0800

    [feat][caleb] adds profile components

--- src/components/Profile/NotificationComponent.tsx
+++ src/components/Profile/NotificationComponent.tsx
@@ -1,8 +1,12 @@
 const NotificationComponent = () => {
     return (
         console.log("NotificationComponent rendered"),
-        <div className="bg-white w-full max-w-[90%] shadow-[0px_4px_20px_0px_rgba(0,0,0,0.2)] rounded-3xl p-4">
-            {/* Content goes here */}
+        <div className="bg-white w-full max-w-[90%] shadow-[0px_4px_20px_0px_rgba(0,0,0,0.2)] rounded-xl p-4 text-black">
+            <h1>Commented on a Forum</h1>
+            <p>Lorem ipsum dolor sit amet</p>
+            <p>Lorem ipsum dolor sit amet</p>
+            <p>Lorem ipsum dolor sit amet</p>
+
         </div>
     );
 };
