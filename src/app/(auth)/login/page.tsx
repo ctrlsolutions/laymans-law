d18b29d31b5ddffad05cf4416673dd6f61d4eef5
Az <arbusinos@up.edu.ph>
2025-03-20 16:17:26 +0800

    [feat][az] modularizes log in

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,19 @@
 import AuthContainer from "@/components/Auth/AuthContainer";
+import LoginForm from "@/components/Auth/LogInForm";
 import Login from "./login";
 
 export default function LoginPage() {
   return (
     <AuthContainer bgColor="black">
-      <Login />
+      <div className="h-fit w-full flex flex-col gap-9 my-8 px-6 sm:gap-3 items-center">
+        <h1 className="text-3xl sm:text-4xl lg:text-5xl font-extrabold text-black">
+          Login
+        </h1>
+        <p className="text-gray-600 sm:text-xl">
+          Welcome back! Please log in to your account.
+        </p>
+        <LoginForm />
+      </div>
     </AuthContainer>
   );
 }
