cc3cdf5a5caf8c7d3eb677fe4ca33107f7361fb8
ndb3llo <ndbello@up.edu.ph>
2025-03-20 19:21:27 +0800

    [feat][Nico] Creates profile and statistics components

--- src/app/dashboard/account/page.tsx
+++ src/app/dashboard/account/page.tsx
@@ -1,17 +1,38 @@
-// this is used by layout.tsx
-import UserProfile from "./Information";
-import Notifications from "./notifications";
-import Cases from "./cases";
+import CaseContainer from "@/components/Profile/CaseContainer";
+import LawyerActiveCases from "@/components/Profile/LawyerActiveCases";
+import InformationComponent from "@/components/Profile/InformationComponent";
+import NotificationComponent from "@/components/Profile/NotificationComponent";
+import NotificationContainer from "@/components/Profile/NotificationContainer";
+import Statistics from "@/components/Profile/statistics";
+
+const userData = {
+    name: "LeBron James",
+    avatar: '/DefaultProfile.png',
+    role: "Layman",
+    address: "Los Angeles, California, USA",
+    email: "kingjames@gmail.com",
+    phone: "09615727576",
+    gender: "Male",
+    birthdate: "December 12, 2003",
+}
+
 
 const Home = () => {
     
     return (
         <>
-        <div className="w-full h-full flex flex-col flex-wrap justify-between content-between">
-            <UserProfile />
-            <Notifications />
-            <Cases />
-        </div>
+            <div className="grid grid-cols-[40%_1fr] grid-row-2 gap-3 h-[90vh]">
+                <div className="row-span-1 flex flex-col gap-3">
+                    <InformationComponent userData={userData}/>
+                    <Statistics />
+                </div>
+                
+                <div className="col-span-1">
+                    <CaseContainer count={12}>
+                            <LawyerActiveCases />
+                    </CaseContainer>
+                </div>
+            </div>
         </>
     );
     
