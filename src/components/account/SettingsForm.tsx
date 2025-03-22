be41d50b41a36be5548869700f4d5599cd774783
Erl <136324144+ermontano@users.noreply.github.com>
2025-03-22 16:43:51 +0800

    [Feat][Erl] Setting Form layman with usertype

--- src/components/account/SettingsForm.tsx
+++ src/components/account/SettingsForm.tsx
@@ -2,10 +2,14 @@
 
 import { useState } from "react";
 import BaseFormInput from "@/components/Global/BaseFormInput";
-import { handleInputChange } from "@/utils/AuthUtils";
+import BaseFormSelect from "@/components/Global/BaseFormSelect";
 import Button from "@/components/Global/BaseButton";
 
-export default function Home() {
+interface SettingsFormProps {
+  userType: "layman" | "lawyer";
+}
+
+export default function Home({ userType }: SettingsFormProps) {
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
@@ -14,12 +18,14 @@ export default function Home() {
     address: "",
     occupation: "",
     gender: "male",
+    rollNumber: "",
+    rolldate: ""
   });
 
   const handleChange = (
     event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
   ) => {
-    handleInputChange<SignupData>(event, setForm, setErrors, validateField);
+    setFormData({ ...formData, [event.target.name]: event.target.value });
   };
 
   const handleSaveChanges = () => {
@@ -56,7 +62,7 @@ export default function Home() {
               color="gray-900"
               icon="user"
               value={formData.firstName}
-              onChange={handleInputChange}
+              onChange={handleChange}
             />
 
             <BaseFormInput
@@ -66,7 +72,7 @@ export default function Home() {
               color="gray-900"
               icon="user"
               value={formData.lastName}
-              onChange={handleInputChange}
+              onChange={handleChange}
             />
           </div>
 
@@ -77,54 +83,22 @@ export default function Home() {
             color="gray-900"
             icon="tel"
             value={formData.contactNumber}
-            onChange={handleInputChange}
+            onChange={handleChange}
           />
 
           <div className="grid grid-cols-2 gap-4 mt-2">
-            <div className="w-full">
-              <label className="text-2xl font-bold text-gray-900 mb-2">
-                Sex:
-              </label>
-              <div className="flex gap-1 mt-6">
-                <label
-                  className={`flex items-center w-full h-12 px-4 border rounded-lg cursor-pointer ${
-                    formData.gender === "male"
-                      ? "bg-gray-100 border-gray-900 text-gray-900"
-                      : "border-gray-300 text-gray-800"
-                  }`}
-                  onClick={() => setFormData({ ...formData, gender: "male" })}
-                >
-                  <div
-                    className={`w-6 h-6 rounded-full border ${
-                      formData.gender === "male"
-                        ? "bg-red border-red"
-                        : "border-gray-400"
-                    } mr-2`}
-                  />
-                  Male
-                </label>
+            <BaseFormSelect
+              label="Gender"
+              name="gender"
+              value={formData.gender}
+              choices={[
+                { label: "Male", value: "male" },
+                { label: "Female", value: "female" },
+                { label: "Other", value: "other" },
+              ]}
+              onChange={handleChange}
+            />
 
-                <label
-                  className={`flex items-center w-full h-12 px-4 border rounded-lg cursor-pointer ${
-                    formData.gender === "female"
-                      ? "bg-gray-100 border-gray-900 text-gray-900"
-                      : "border-gray-300 text-gray-800"
-                  }`}
-                  onClick={() => setFormData({ ...formData, gender: "female" })}
-                >
-                  <div
-                    className={`w-6 h-6 rounded-full border ${
-                      formData.gender === "female"
-                        ? "bg-red border-red"
-                        : "border-gray-400"
-                    } mr-2`}
-                  />
-                  Female
-                </label>
-              </div>
-            </div>
-
-            {/* Birthday Field */}
             <BaseFormInput
               label="Birthday"
               name="birthday"
@@ -132,20 +106,10 @@ export default function Home() {
               color="gray-900"
               icon="calendar"
               value={formData.birthday}
-              onChange={handleInputChange}
+              onChange={handleChange}
               className="appearance-none px-4 py-2 border border-gray-300 rounded-lg text-gray-900 w-full h-11"
             />
           </div>
-
-          <BaseFormInput
-            label="Address:"
-            name="address"
-            type="text"
-            color="gray-900"
-            icon="hash"
-            value={formData.address}
-            onChange={handleInputChange}
-          />
 
           <BaseFormInput
             label="Occupation"
@@ -154,8 +118,34 @@ export default function Home() {
             color="gray-900"
             icon="hash"
             value={formData.occupation}
-            onChange={handleInputChange}
+            onChange={handleChange}
           />
+
+          {userType === "lawyer" && (
+            <div className="grid grid-cols-2 gap-4 mt-4">
+              <BaseFormInput
+                label="Roll Number:"
+                name="rollNumber"
+                type="text"
+                color="gray-900"
+                icon="hash"
+                value={formData.rollNumber}
+                onChange={handleChange}
+                className="font-bold text-gray-800 bg-gray-100 border border-gray-300 rounded-lg px-4 py-2 w-full"
+              />
+
+              <BaseFormInput
+                label="Roll Signed Date:"
+                name="rolldate"
+                type="date"
+                color="gray-900"
+                icon="calendar"
+                value={formData.rolldate}
+                onChange={handleChange}
+                className="font-bold text-gray-800 bg-gray-100 border border-gray-300 rounded-lg px-4 py-2 w-full"
+              />
+            </div>
+          )}
         </div>
       </div>
       <div className="flex justify-between text-sm mt-10">
