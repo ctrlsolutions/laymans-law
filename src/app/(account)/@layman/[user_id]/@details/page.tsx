a2ce1f702c499143519b80e31204139d7f2dda2d
Caleb <calebjosh.berandoy@gmail.com>
2025-03-23 17:48:11 +0800

    [feat][caleb] adds fetch for details component

--- src/app/(account)/@layman/[user_id]/@details/page.tsx
+++ src/app/(account)/@layman/[user_id]/@details/page.tsx
@@ -1,9 +1,36 @@
 "use client";
+import { useEffect, useState } from "react";
+import InformationComponent from "@/components/Profile/InformationComponent";
+import { getProfile } from "@/services/ProfileServices";
+
 export default function LaymanDetails() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+        const response = await getProfile();
+        if (response.success && response.data) {
+            setUser(response.data);
+        } else {
+            console.error("Error fetching user data:", response.message);
+        }
+        setLoading(false);
+    };
+
+    fetchUserProfile();
+  }, []);
+
     return (
       <>
         <div className="w-full bg-red-500 h-full flex flex-col flex-wrap justify-between content-between">
-          LAYMAN DETAILS
+          {loading ? (
+              <p>Loading...</p>
+          ) : user ? (
+              <InformationComponent userData={user} />
+          ) : (
+              <p>Error loading user data.</p>
+          )}
         </div>
       </>
     );
