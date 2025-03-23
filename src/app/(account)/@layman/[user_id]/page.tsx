5d476d8b338405610349c5d56fe1ce9ed7692519
Caleb <calebjosh.berandoy@gmail.com>
2025-03-23 16:10:36 +0800

    [feat][caleb] fixes merge

--- src/app/(account)/@layman/[user_id]/page.tsx
+++ src/app/(account)/@layman/[user_id]/page.tsx
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-"use client";
-export default function ProfileLayout({
-    details,
-    notifications,
-    submitted_cases,
-}: {
-    lawyer: React.ReactNode;
-    layman: React.ReactNode;
-    details: React.ReactNode;
-    notifications: React.ReactNode;
-    submitted_cases: React.ReactNode;
-}) {
-  return (
-    <>
-        {details}
-        {notifications}
-        {submitted_cases}
-    </>
-  );
-}
-=======
 export default function LaymanUserPage({ children }: { children: React.ReactNode }) {
     return <>{children}</>;
   }
->>>>>>> 4857682 ([feat][caleb] routing)
