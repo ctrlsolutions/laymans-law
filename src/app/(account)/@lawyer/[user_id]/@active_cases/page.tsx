bd3424ce7b7ddaac21e387348846f42d5e47f9f0
Caleb <calebjosh.berandoy@gmail.com>
2025-03-31 11:24:40 +0800

    [feat][caleb] fixes lawyer profile layout

--- src/app/(account)/@lawyer/[user_id]/@active_cases/page.tsx
+++ src/app/(account)/@lawyer/[user_id]/@active_cases/page.tsx
@@ -6,7 +6,7 @@ import Card from "@/components/Profile/Card";
 export default function LawyerActiveCases() {
     return (
       <>
-        <Card className="h-[61.5vh]">
+        <Card className="h-[53vh]">
           <ActiveCasesContainer count={5}>
             <ActiveCasesComponent />
           </ActiveCasesContainer>
