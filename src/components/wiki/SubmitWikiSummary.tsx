43f8d3af86a92e8706b56bcc8aec9133c9caeb55
Erl <136324144+ermontano@users.noreply.github.com>
2025-04-05 17:11:56 +0800

    [Feat][Erl] Submit Wiki Summary

--- src/components/wiki/SubmitWikiSummary.tsx
+++ src/components/wiki/SubmitWikiSummary.tsx
@@ -4,13 +4,35 @@ import React, { useState } from 'react';
 import LeftPanel from '@/components/wiki/LeftPanelSummary';
 import RightPanel from '@/components/wiki/RightPanelSummary';
 import Button from '@/components/Global/BaseButton';
+import BaseFormSelect from '@/components/Global/BaseFormSelect';
 
 function App() {
   const [selectedChapter, setSelectedChapter] = useState('1');
+  const [submissionType, setSubmissionType] = useState('Summary');
+
+  const handleSubmissionTypeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSubmissionType(e.target.value);
+  };
+
+  const submissionTypeChoices = [
+    { value: 'Summary', label: 'Summary' },
+    { value: 'Translation', label: 'Translation' },
+  ];
 
   return (
     <div className="w-full h-full flex flex-col p-6 sm:p-6">
-      <h1 className="text-lg font-medium mb-4 text-black">Submit Summarization</h1>
+      <div className="flex items-center justify-between mb-4">
+        <h1 className="text-lg font-medium text-black">Submit Summarization</h1>
+        <BaseFormSelect
+          label=" "
+          name="submissionType"
+          value={submissionType}
+          choices={submissionTypeChoices}
+          onChange={handleSubmissionTypeChange}
+          color="blue"
+          width="w-60"
+          > Summary </BaseFormSelect>
+      </div>
       <hr className="mb-9" />
 
       <div className="flex flex-col md:flex-row gap-6 text-black flex-grow">
@@ -21,13 +43,12 @@ function App() {
         <RightPanel selectedChapter={selectedChapter} />
       </div>
 
-      {/* Buttons aligned with the right container */}
-      <div className="flex justify-center md:w-2/3 ml-auto gap-40 pt-4 border-t border-gray-100 mt-2">
-      <Button color="red" width="18rem" onClick={() => console.log('Cancel')}>
-        Cancel Submission
+      <div className="flex justify-center md:w-2/3 ml-auto gap-40 pt-4 mt-2">
+        <Button color="red" width="18rem" onClick={() => console.log('Cancel')}>
+          Cancel Submission
         </Button>
         <Button color="blue" width="18rem" onClick={() => console.log('Submit')}>
-        Submit Summarization
+          Submit Summarization
         </Button>
       </div>
     </div>
