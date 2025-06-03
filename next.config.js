6447bc9269833f497242b43e25dbea075eb55abb
Lanieza <lblopez@up.edu.ph>
2025-06-03 20:12:59 +0800

    adds localhost in image domains in config ts

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'cdn.builder.io',
-      'via.placeholder.com'
-    ],
+    domains: ["cdn.builder.io", "via.placeholder.com", "127.0.0.1"],
   },
 };
 
-module.exports = nextConfig; 
+module.exports = nextConfig;
