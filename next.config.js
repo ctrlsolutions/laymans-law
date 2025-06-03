a2db30ef778176336e244df3bc11085b4c6ff1a0
Dallss <raalquicer@up.edu.ph>
2025-06-04 00:39:32 +0800

    Update image domains configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.builder.io", "via.placeholder.com", "127.0.0.1"],
+    domains: [
+      "cdn.builder.io",
+      "via.placeholder.com",
+      "127.0.0.1",
+      "localhost",
+      ...(process.env.NEXT_PUBLIC_IMAGE_DOMAINS ? process.env.NEXT_PUBLIC_IMAGE_DOMAINS.split(',') : [])
+    ],
   },
 };
 
